3a9483ab557a997c72cdf3addb8ac37831e2ea1d
Adailton Rodriguez <bodoillnerr@gmail.com>
2018-10-29 10:15:35 -0600

    login y borrador

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,7 @@
 from django import forms
-
 from .models import Publicacion
 
-class NuevoForm(forms.ModelForm):
-
-    class Meta:
-        model = Publicacion
-        fields = ('titulo', 'texto',)
+class PostForm(forms.ModelForm):
+        class Meta:
+            model = Publicacion
+            fields = ('titulo', 'texto',)
--- misitio/urls.py
+++ misitio/urls.py
@@ -1,8 +1,13 @@
-
-from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.contrib.auth import views
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+    url(r'',include('blog.urls')),
 ]
