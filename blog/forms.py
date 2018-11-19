a3b8fbedd48044a758ad05cd75c70130827c493e
Adailton Rodriguez <bodoillnerr@gmail.com>
2018-11-19 10:47:14 -0600

    Formularios

--- blog/forms.py
+++ blog/forms.py
@@ -1,20 +1,20 @@
 from django import forms
-from .models import Paciente, Doctor
+from .models import Grado, Materia
 
 class PostForm1(forms.ModelForm):
 
         class Meta:
-            model = Paciente
-            fields = ('nombre_paciente', 'direccion_paciente', 'telefono_paciente', 'correo_paciente','sintomas_paciente', 'doctores')
+            model = Grado
+            fields = ('nombre_grado', 'seccion_grado', 'materias')
 
             def __init__ (self, *args, **kwargs):
                 super(PostForm3, self).__init__(*args, **kwargs)
-                self.fields["doctores"].widget = forms.widgets.CheckboxSelectMultiple()
-                self.fields["doctores"].help_text = "Seleccione al doctor"
-                self.fields["doctores"].queryset = Doctor.objects.all()
+                self.fields["materias"].widget = forms.widgets.CheckboxSelectMultiple()
+                self.fields["materias"].help_text = "Seleccione las material"
+                self.fields["materias"].queryset = Materia.objects.all()
 
 class PostForm2(forms.ModelForm):
 
         class Meta:
-            model = Doctor
-            fields = ('nombre_doctor', 'direccion_doctor', 'telefono_doctor', 'correo_doctor','colegiado_doctor','especialidad_doctor')
+            model = Materia
+            fields = ('nombre_materia', 'creditos_materia', 'profesor_materia')
